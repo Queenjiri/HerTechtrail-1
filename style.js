049f1300cc82dc28d22a48f6e1b728b0402ec840
Queenjiri <ajirijohnsonblessing@gmail.com>
2023-03-30 04:49:36 +0100

    This Fitness is for Techtrail cohort6 project

---
+++ style.js
@@ -0,0 +1,88 @@
+const firstImage = document.getElementById('image1');
+const imageOne = document.getElementById('image2');
+const imageTne = document.getElementById('image3');
+const imageBar = document.getElementById('image4');
+const imageFour = document.getElementById('image5');
+const imageFive = document.getElementById('image6');
+const imageSix = document.getElementById('image7');
+const imageSeven = document.getElementById('image8');
+const imageEight = document.getElementById('image9');
+const imageNine = document.getElementById('image10');
+const imageTen = document.getElementById('image11');
+const imageEleven = document.getElementById('image12');
+
+image1.addEventListener('click', (e)=>{
+e.target.style.background = '#111';
+e.target.style.color='white';
+});
+
+
+
+image2.addEventListener('click', (e)=>{
+    e.target.style.backgroundColor = 'blue';
+    e.target.style.color='white';
+    });
+
+    
+image3.addEventListener('click', (event)=>{
+    event.target.style.background = 'red';
+    event.target.style.color='white';
+    });
+
+    
+    
+image4.addEventListener('click', ( event)=>{
+    event.target.style.background = 'indigo';
+    event.target.style.color='white';
+    });
+
+    
+image5.addEventListener('click', ( event)=>{
+    event.target.style.background = 'yellow';
+    event.target.style.color='#111';
+    });
+    
+    
+image6.addEventListener('click', (e)=>{
+    e.target.style.background = 'purple';
+    e.target.style.color='white';
+    });
+    
+    
+image7.addEventListener('click', (e)=>{
+    e.target.style.background = 'pink';
+    e.target.style.color='#111';
+    });
+    
+    
+image8.addEventListener('click', (e)=>{
+    e.target.style.background = 'green';
+    e.target.style.color='white';
+    });
+    
+    
+image9.addEventListener('click', (e)=>{
+    e.target.style.background = 'gold';
+    e.target.style.color='white';
+    });
+    
+    
+image10.addEventListener('click', (e)=>{
+    e.target.style.background = 'red';
+    e.target.style.color='white';
+    });
+    
+    
+image11.addEventListener('click', (e)=>{
+    e.target.style.background = 'orange';
+    e.target.style.color='white';
+    });
+    
+    
+image12.addEventListener('click', (e)=>{
+    e.target.style.background = 'brown';
+    e.target.style.color='white';
+    });
+    
+
+
